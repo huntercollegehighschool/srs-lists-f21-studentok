b7259008de12f287f99c69eb39bc55967de0ad2e
studentok <74572678+studentok@users.noreply.github.com>
2021-11-12 17:28:20 +0000

    Completed activities

--- Program2.py
+++ Program2.py
@@ -5,4 +5,10 @@ PROGRAM 2
 Define a function odd_reverse that takes a list as an argument. The function creates a new list containing only the elements in the odd indices (indices 1, 3, 5, ...) and then reverses it. The function returns that list.
 '''
 def odd_reverse(lst):
-  pass #delete this line when you start writing your code
+  odds = []
+  length = len(lst)
+  for i in range(length):
+    if i%2 != 0:
+      odds.append(lst[i])
+  odds.reverse()
+  return odds 
--- Program4.py
+++ Program4.py
@@ -7,4 +7,10 @@ Define a function above_average that takes a list of integers or floats as an ar
 You may define a separate function that finds the average of a list, though you don't have to.
 '''
 def above_average(lst):
-  pass #delete this line when you start writing your code
+  mean = sum(lst)/len(lst)
+  above = []
+  for i in lst:
+    if i > mean:
+      above.append(i)
+  return above 
+
